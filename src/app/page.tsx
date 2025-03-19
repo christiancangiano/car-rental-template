bb6c53a045ff49040b938a9f52f3175d35cec5df
christiancangiano <christiancangiano2@gmail.com>
2025-03-19 11:43:02 -0400

    changes to the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -43,17 +43,17 @@ const HomePage = () => {
         </motion.div>
       </section>
 
-      {/* Car Carousel Section */}
+      
       <section data-navbar-color="light" className="min-h-screen flex items-center justify-center bg-gray-600 w-full overflow-hidden">
         <CarCarousel />
       </section>
 
-      {/* Car Rentals Section */}
+      
       <section className="min-h-screen flex flex-col items-center justify-center bg-gray-100 px-6 py-12 w-full">
         <h2 className="text-4xl font-bold mb-8 text-gray-600 text-center">Luxury Car Rentals</h2>
 
         <div className="flex flex-col md:flex-row w-full max-w-6xl justify-center items-center gap-4">
-          {/* NYC Rental Card */}
+          
           <Link href="/services/newyork">
             <div className="w-full md:w-1/2 bg-white shadow-lg rounded-xl overflow-hidden flex flex-col cursor-pointer">
               <Image src="/nyc.png" alt="New York City Car Rental" width={600} height={400} className="w-full h-64 object-cover" />
@@ -63,7 +63,7 @@ const HomePage = () => {
             </div>
           </Link>
 
-          {/* LA Rental Card */}
+          
           <Link href="/services/losangeles">
             <div className="w-full md:w-1/2 bg-white shadow-lg rounded-xl overflow-hidden flex flex-col cursor-pointer">
               <Image src="/hollywood.png" alt="Los Angeles Car Rental" width={600} height={400} className="w-full h-64 object-cover" />
@@ -75,7 +75,7 @@ const HomePage = () => {
         </div>
       </section>
 
-      {/* Testimonials Section */}
+      
       <section className="min-h-screen flex flex-col items-center justify-center bg-gray-600 px-4 w-full overflow-hidden">
         <h2 className="text-4xl font-bold mb-6 text-center">What Our Clients Say</h2>
         <TestimonialCarousel />
