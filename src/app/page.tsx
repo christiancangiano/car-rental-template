f87779f12b81457a15fdfe4b4a5e4dfbf1119de0
christiancangiano <christiancangiano2@gmail.com>
2025-03-24 13:10:56 -0400

    created our partners section on the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import TestimonialCarousel from "@/components/TestimonialCarousel";
 import Footer from "@/components/Footer"; 
 import Link from "next/link";
 
+
 const HomePage = () => {
   return (
     <>
@@ -50,7 +51,7 @@ const HomePage = () => {
 
       
       <section className="min-h-screen flex flex-col items-center justify-center bg-gray-100 px-6 py-12 w-full">
-  <h2 className="text-4xl font-bold mb-8 text-gray-600 text-center">Luxury Car Rentals</h2>
+  <h2 className="text-4xl font-bold mb-8 text-black text-center">Luxury Car Rentals</h2>
 
   <div className="flex flex-col md:flex-row w-full max-w-6xl justify-center items-center gap-6">
     
@@ -59,8 +60,8 @@ const HomePage = () => {
       <div className="bg-white shadow-lg rounded-xl overflow-hidden flex flex-col cursor-pointer">
         <Image src="/nyc.png" alt="New York City Car Rental" width={600} height={400} className="w-full h-64 object-cover" />
         <div className="p-6 text-center">
-          <h3 className="text-2xl font-bold text-gray-600 mb-4">New York City</h3>
-          <p className="text-gray-600 text-base">
+          <h3 className="text-2xl font-bold text-black mb-4">New York City</h3>
+          <p className="text-black text-base">
             Cruise through the streets of NYC in style with our luxury car rentals.
             Whether you're exploring Manhattan or cruising through Brooklyn, 
             we offer top-tier supercars for every occasion.
@@ -74,8 +75,8 @@ const HomePage = () => {
       <div className="bg-white shadow-lg rounded-xl overflow-hidden flex flex-col cursor-pointer">
         <Image src="/hollywood.png" alt="Los Angeles Car Rental" width={600} height={400} className="w-full h-64 object-cover" />
         <div className="p-6 text-center">
-          <h3 className="text-2xl font-bold text-gray-600 mb-4">Los Angeles</h3>
-          <p className="text-gray-600 text-base">
+          <h3 className="text-2xl font-bold text-black mb-4">Los Angeles</h3>
+          <p className="text-black text-base">
             Experience Hollywood glamour with our luxury rentals.
             From Beverly Hills to the Pacific Coast Highway, drive the world’s most exclusive supercars in LA.
           </p>
@@ -92,6 +93,85 @@ const HomePage = () => {
         <TestimonialCarousel />
       </section>
 
+      <section className="min-h-screen flex flex-col items-center justify-center bg-white px-4 w-full overflow-hidden">
+      <h2 className="text-5xl font-bold mb-6 h-20 text-black text-center">Our Partners</h2>
+      <div className="flex flex-wrap justify-center gap-8">
+      <div className="relative h-24 w-36 sm:h-28 sm:w-40 md:h-32 md:w-48">
+      <Image
+        src="/centurion.png"
+        alt="Centurion"
+        fill
+        className="object-contain"
+      />
+    </div>
+    <div className="relative h-24 w-36 sm:h-35 sm:w-46 md:h-35 md:w-48 gap-8">
+    <Image
+        src="/sapphirelogo.png"
+        alt="Sapphire"
+        fill
+        className="object-contain"
+      />
+    </div>
+    <div className="relative h-60 w-76 sm:h-35 sm:w-46 md:h-35 md:w-48 gap-8">
+    <Image
+        src="/ferraripalmbeach.png"
+        alt="Ferrari Palm Beach"
+        fill
+        className="object-contain"
+      />
+
+    </div>
+    <div className="relative h-24 w-36 sm:h-35 sm:w-46 md:h-35 md:w-48 gap-8">
+    <Image
+        src="/lambomiami.png"
+        alt="Lamborghini Miami"
+        fill
+        className="object-contain"
+      />
+    </div>
+    <div className="relative h-24 w-36 sm:h-35 sm:w-46 md:h-35 md:w-48 gap-8">
+    <Image
+        src="/nfcu.png"
+        alt="Navy Federal"
+        fill
+        className="object-contain"
+      />
+    </div>
+    <div className="relative h-24 w-36 sm:h-35 sm:w-46 md:h-35 md:w-48 gap-8">
+    <Image
+        src="/geico.png"
+        alt="Geico"
+        fill
+        className="object-contain"
+      />
+    </div>
+    <div className="relative h-24 w-36 sm:h-35 sm:w-46 md:h-35 md:w-48 gap-8">
+    <Image
+        src="/mbca.png"
+        alt="Mercedes"
+        fill
+        className="object-contain"
+      />
+    </div>
+    <div className="relative h-24 w-36 sm:h-35 sm:w-46 rounded bg-black md:h-35 md:w-48 gap-8">
+    <Image
+        src="/mclaren.png"
+        alt="Mclaren"
+        fill
+        className="object-contain"
+      />
+    </div>
+    <div className="relative h-24 w-36 sm:h-35 sm:w-46 md:h-35 md:w-48 gap-8">
+    <Image
+        src="/bentleylogo.png"
+        alt="Bentley"
+        fill
+        className="object-contain"
+      />
+    </div>
+
+      </div>
+      </section>
       <Footer />
     </>
   );
